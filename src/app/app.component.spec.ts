580484482739c96878a303a533d6f3815e760141
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2021-04-27 13:18:30 +0900

    angular-asyncをangular-async-meansに編集

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-async'`, () => {
+  it(`should have as title 'angular-async-means'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-async');
+    expect(app.title).toEqual('angular-async-means');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-async app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-async-means app is running!');
   });
 });
